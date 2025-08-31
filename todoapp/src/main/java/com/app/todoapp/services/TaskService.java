818422df785fd186a12dfaee37e2d94c770b5fc8
Dibs07 <banerjeedibs@gmail.com>
2025-08-31 08:06:08 +0530

    Todo app

---
+++ todoapp/src/main/java/com/app/todoapp/services/TaskService.java
@@ -0,0 +1,43 @@
+package com.app.todoapp.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.app.todoapp.models.Task;
+import com.app.todoapp.repository.TaskRepository;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void createTask(String title, String description) {
+        Task task = new Task();
+        task.setTitle(title);
+        task.setDescription(description);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void toggleTask(Long id) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Task not found"));
+        task.setCompleted(!task.isCompleted());
+        taskRepository.save(task);
+    }
+
+    
+
+}
